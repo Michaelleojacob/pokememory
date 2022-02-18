c04eb0e9e2210d9dd487a22f6957aa0743626267
Michael <michaelleojacob@gmail.com>
2022-02-18 14:53:32 -0500

    I think I finally have the header the way I want it. Lets just hope it stays and doesn't keep mutating seemingly randomly

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import CircleType from 'circletype';
 
 const Header = () => {
+  const circleInstance = useRef();
+
+  useEffect(() => {
+    new CircleType(circleInstance.current).radius(400);
+  }, []);
+
   return (
-    <div>
-      <div>poke-memory</div>
+    <div id="header-container">
+      <img id="header-img" src={'/images/pokeball.jpeg'} alt="pokeball"></img>
+      <div id="header-title">
+        <div ref={circleInstance} id="header-txt">
+          pokememory
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/components/intructions.js
+++ src/app/components/intructions.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Instructions = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const renderIsOpen = () => {
+    return (
+      <div id="instructions-title">
+        how to play:
+        <div id="instructions-text">
+          Begin the game by clicking on a pokemon.
+          <br /> Click on different pokemon to win.
+          <br /> There are 12 unique pokemon, try to click each one once.
+        </div>
+        <button onClick={handleClick}>hide instructions</button>
+      </div>
+    );
+  };
+
+  const renderIsClosed = () => {
+    return (
+      <div>
+        <button onClick={handleClick}>show instructions</button>
+      </div>
+    );
+  };
+
   return (
-    <div>
-      <div>how to play: </div>
-    </div>
+    <div id="instructions">{isOpen ? renderIsOpen() : renderIsClosed()}</div>
   );
 };
 
